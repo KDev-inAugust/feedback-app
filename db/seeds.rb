d7029a9507426bfb0683aa270b2e4675afca32f4
Kane Mathis <admin@kanemathis.com>
2023-03-26 17:12:38 -0400

    seed example client projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,18 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
-User.all.destroy
-Project.all.destroy
 
-u1=User.create(name: "UserOne", password: "password1")
-u2=User.create(name: "UserTwo", password: "password2")
-u3=User.create(name: "UserThree", password: "password3")
 
-p1=Project.create(name: "Project One", user_id: 1)
-p2=Project.create(name: "Project Two", user_id: 1)
-p3=Project.create(name: "Project Three", user_id: 2)
-p4=Project.create(name: "Project Four", user_id: 2)
-p5=Project.create(name: "Project Five", user_id: 3)
-p6=Project.create(name: "Project Six", user_id: 3)
+# u1=User.create(name: "UserOne", password: "password1")
+# u2=User.create(name: "UserTwo", password: "password2")
+# u3=User.create(name: "UserThree", password: "password3")
+
+# p1=Project.create(name: "Project One", user_id: 1)
+# p2=Project.create(name: "Project Two", user_id: 1)
+# p3=Project.create(name: "Project Three", user_id: 2)
+# p4=Project.create(name: "Project Four", user_id: 2)
+# p5=Project.create(name: "Project Five", user_id: 3)
+# p6=Project.create(name: "Project Six", user_id: 3)
 
 cp=ClientProject.create(user_id: 1, project_id: 3)
 cp=ClientProject.create(user_id: 1, project_id: 4)
