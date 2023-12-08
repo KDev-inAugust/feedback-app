5bd2120ba1aa1a051cdfe9f0c97601488ef38d4a
Kane Mathis <admin@kanemathis.com>
2023-12-08 09:44:19 -0500

    file management for namespacing

--- app/controllers/active_storage_attachments_controller.rb
+++
@@ -1,8 +0,0 @@
-class ActiveStorageAttachmentsController < ApplicationController
-    before_action :authorize
-    def index 
-        asa=ActiveStorageAttachment.all
-        render json: asa
-    end
-
-end
