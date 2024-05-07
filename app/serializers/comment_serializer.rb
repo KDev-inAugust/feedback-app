0473dc39e8fa2b82408463a7f6e5764ebb06430c
Kane Mathis <admin@kanemathis.com>
2024-05-07 12:35:23 -0400

    fix typo in comment serializer

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :active_storage_attachment_id, :user_id, :track_time, :body, :user_name, project_file_id
+  attributes :id, :active_storage_attachment_id, :user_id, :track_time, :body, :user_name, :project_file_id
 
   
 end
